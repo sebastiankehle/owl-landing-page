a12e1bd543a74f34a8aa361ce56e5a4340a96883
sebastiankehle <mail@webvise.io>
2024-12-30 19:41:15 +0100

    fix footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,15 +2,13 @@ import { Nav } from "./nav";
 
 interface FooterProps {
   dictionary: {
-    header: {
+    footer: {
       nav: {
         home: string;
         about: string;
         blog: string;
         contact: string;
       };
-    };
-    footer: {
       copyright: string;
     };
   };
@@ -20,7 +18,7 @@ export function Footer({ dictionary }: FooterProps) {
   return (
     <footer className="border-t border-border/10 bg-background">
       <div className="container flex flex-col items-center py-16">
-        <Nav dictionary={dictionary.header.nav} variant="footer" />
+        <Nav dictionary={dictionary.footer.nav} variant="footer" />
         <div className="mt-8 text-sm text-muted-foreground">
           {dictionary.footer.copyright}
         </div>
